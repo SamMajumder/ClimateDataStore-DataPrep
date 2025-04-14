99ea22a66fa7937ca19bdcea9708ab7f63b4250f
SamMajumder <sammajumder692@gmail.com>
2025-04-14 14:11:57 -0400

    initial commit

---
+++ era5-monthly-averages/download.py
@@ -0,0 +1,27 @@
+
+
+###########
+### Read a shapefile ###
+####### 
+
+
+
+
+
+
+
+######
+### extract the ROI ### 
+###### 
+
+
+
+############
+### Orient the ROI to how CDS expects it
+######### 
+
+
+##### 
+## Use this ROI to download the data using custom python function with parameters 
+##### 
+
